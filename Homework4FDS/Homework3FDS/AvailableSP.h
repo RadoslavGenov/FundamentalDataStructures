0b6f49094e17a7c4da4cbcf029193f372fd989f8
Radoslav Genov <radoslavgenov5@hotmail.com>
2016-02-25 22:33:28 +0200

    4th Homework for FDS copyright Radoslav Genov

---
+++ Homework4FDS/Homework3FDS/AvailableSP.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef AVAILABLESP_H
+#define AVAILABLESP_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include "Vehicle.h"
+#include "SparePart.h"
+
+using namespace std;
+
+class Vehicle;
+class SparePart;
+
+class AvailableSP : public SparePart//Available Spare Part class
+{
+public:
+	AvailableSP()
+		: Location("no location") {}
+	void addSparePart();
+	void printAvailablePart();
+	void quickSort(vector<SparePart*>& partsList, int low, int high);
+	void insertionSort(vector<SparePart*>& AvailableSP, int low, int high);
+	void swap(SparePart* a, SparePart* b);
+	string getLocation() { return Location; }
+protected:
+	string Location;
+};
+
+void AvailableSP::addSparePart() {//add location with call to base function
+	SparePart::addSparePart();
+	cout << "Enter location:";
+	cin >> Location;              //set location
+}
+
+void AvailableSP::printAvailablePart() {//simple function to print available parts list
+	cout << "ID:   Name:   Quantity:   Price:   Status:   Location:   Producer:   Model:" << endl;
+	for (int i = 0; i < (int)partsList.size(); i++)
+	{
+		cout << i << setw(10)
+			<< partsList.at(i)->getName() << setw(10)
+			<< partsList.at(i)->getQuantity() << setw(10)
+			<< partsList.at(i)->getPrice() << setw(10)
+			<< partsList.at(i)->getStatus() << setw(10)
+			<< partsList.at(i)->getLocation() << setw(10)
+			<< partsList.at(i)->getProducer() << setw(10)
+			<< partsList.at(i)->getModel() << endl;
+	}
+}
+
+#endif
